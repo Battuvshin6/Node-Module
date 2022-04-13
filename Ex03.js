36661fcba8850b9e5aaba33aa73ef570c3040042
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-04-13 14:55:53 +0800

    [ADD] exercise 3 done

---
+++ Ex03.js
@@ -0,0 +1,15 @@
+function ex03(a, b) {
+  if (a === b) {
+    return (a + b) * 3;
+  } else {
+    return a + b;
+  }
+}
+function exB(y, z) {
+  if (a === b) {
+    return (a + b) * 3;
+  } else {
+    return a + b;
+  }
+}
+console.log(ex03(5, 5));
