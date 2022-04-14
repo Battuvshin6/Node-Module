b7d8caa5ef137e7a9f0e318cfe20c19873807c8b
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-04-14 14:48:58 +0800

    [ADD] ex3 done

--- Day-3/ex3.js
+++ Day-3/ex3.js
@@ -1,1 +1,6 @@
-const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const array = [1, 2, 3, 4, 5];
+const newarr = array.map(myFunction);
+function myFunction(value) {
+  return value + 2;
+}
+console.log(newarr);
