68d28f0f04a455a86ab40a39b8fbcb66d14a8d3c
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-04-27 18:15:14 +0800

    [ADD] day-6 ex-3 done

---
+++ Day-6/Ex-3.js
@@ -0,0 +1,30 @@
+let https = require("https");
+let http = require("http");
+let fs = require("fs");
+
+http.createServer(function (req, res) {
+  https
+    .get("https://ghibliapi.herokuapp.com/films", (response) => {
+      let data = [];
+      response.on("data", (news) => {
+        data.push(news);
+      });
+      response.on("end", () => {
+        const news1 = JSON.parse(Buffer.concat(data).toString());
+        const tableBegin = "<table>";
+        const tableEnd = "</table>";
+        let content = "";
+
+        news1.map((e) => {
+          content =
+            content +
+            `<tr ><td >${e.title}</td> <td ><img style=' width: 100px' src="${e.image}"/></td></tr>`;
+        });
+        const result = tableBegin + content + tableEnd;
+        res.end(result);
+      });
+    })
+    .on("error", (err) => {
+      console.log("Error:" + err.message);
+    });
+});
