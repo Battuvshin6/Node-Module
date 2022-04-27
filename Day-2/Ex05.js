f77d44699bcd06d165ad5fda707a010e5cae7ce5
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-04-27 17:58:16 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.

--- Day-2/Ex05.js
+++ Day-2/Ex05.js
@@ -1,7 +1,11 @@
-function ex5(a, b) {
-  if (a + b === 50 < 80) {
+function ex5(x, y) {
+  const sum_nums = x + y;
+  if (sum_nums >= 50 && sum_nums <= 80) {
     return 65;
   } else {
     return 80;
   }
 }
+
+console.log(ex5(5, 10));
+console.log(ex5(100, 1000));
