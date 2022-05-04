74cfe2e57d0657ac62f31dffd5afa6fad4946eb9
Battuvshin <mstarsacademy@MStarss-iMac.local>
2022-05-04 17:50:21 +0800

    [ADD] idk

--- Day-11/app.js
+++ Day-11/app.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require("body-parser");
 const { body, validationResult } = require("express-validator");
 const fs = require("fs");
+app.use(express.static("public"));
 app.use(bodyParser.json());
 app.post(
   "/register",
@@ -17,4 +18,38 @@ app.post(
     }
   }
 );
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("view options");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/404", (req, res) => {
+  res.render("404", { message: "oopsie!" });
+});
+app.get("/js", (req, res) => {
+  let data = {
+    name: "Kaze",
+    hobbies: ["playing basketball", "Esport", "cycling"],
+  };
+  res.render("js", { data: data });
+});
+
+app.get("/photo", (req, res) => {
+  let data = [
+    {
+      name: "Cinnamon Rolls",
+      pic: "/roll.jpeg",
+    },
+    {
+      name: "Donuts",
+      pic: "/donuts.jpeg",
+    },
+    {
+      name: "Pie",
+      pic: "/pie.jpeg",
+    },
+  ];
+  res.render("photo", { data: data });
+});
 app.listen(3000);
